264719241f92177e99956b1181be2cb0ef2c3139
haka1013 <katha_haas@web.de>
2023-05-29 17:17:36 +0200

    Added bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <h2>This is a weather app</h2>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
     </div>
   );
 }
